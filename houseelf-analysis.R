b3393b3215f7eed891418b79431fc23e7239319c
Pablo Moreno Garcia <pablo.paul.paolo@gmail.com>
2016-11-18 13:32:07 -0500

    Downloading the data and adding it to R

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,2 +1,6 @@
 #Analysis to link the elf ear length to the GC content on different house elves individuals
 #in order to study their astonishing magical powers
+
+download.file("http://www.datacarpentry.org/semester-biology/data/houseelf-earlength-dna-data.csv","data/houseelf-earlength-dna-data.csv")
+houseelf_earlength_dna <- read.csv("data/houseelf-earlength-dna-data.csv", stringsAsFactors = F)
+
