2c7fc0b7ceef2911d620533217a76b9a02669457
Pablo Moreno Garcia <pablo.paul.paolo@gmail.com>
2016-11-18 13:51:30 -0500

    Change the names of the datafile (csv and R) because there are multiple files

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,6 +1,6 @@
 #Analysis to link the elf ear length to the GC content on different house elves individuals
 #in order to study their astonishing magical powers
 
-download.file("http://www.datacarpentry.org/semester-biology/data/houseelf-earlength-dna-data.csv","data/houseelf-earlength-dna-data.csv")
-houseelf_earlength_dna <- read.csv("data/houseelf-earlength-dna-data.csv", stringsAsFactors = F)
+download.file("http://www.datacarpentry.org/semester-biology/data/houseelf-earlength-dna-data.csv","data/houseelf-earlength-dna-data_1.csv")
+houseelf_earlength_dna_1 <- read.csv("data/houseelf-earlength-dna-data_1.csv", stringsAsFactors = F) #downloading the first data file
 
